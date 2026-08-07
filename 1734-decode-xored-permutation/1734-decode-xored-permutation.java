e9de3b52452cb935ff5c747b11b095231a8f8d09
Aditya Prakash <adityatejash@gmail.com>
2026-08-07 20:05:03 +0530

    Time: 2 ms (100%), Space: 138 MB (10.46%) - LeetHub

---
+++ 1734-decode-xored-permutation/1734-decode-xored-permutation.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] decode(int[] encoded) {
+        int n = encoded.length + 1;
+
+        int total = 0;
+        for (int i=1; i<=n; i++){
+            total ^= i;
+        }
+
+        int odd = 0;
+        for (int i=1; i<encoded.length; i+=2){
+            odd ^= encoded[i];
+        }
+
+        int[] ans = new int[n];
+        ans[0] = total ^ odd;
+
+        for (int i=0; i<encoded.length; i++){
+            ans[i+1] = ans[i] ^ encoded[i];
+        }
+
+        return ans;
+    }
+}
