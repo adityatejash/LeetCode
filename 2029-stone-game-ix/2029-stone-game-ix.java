2ecf035e90460518854007be81b7a03709148f43
Aditya Prakash <adityatejash@gmail.com>
2026-08-16 15:55:23 +0530

    Time: 7 ms (29.05%), Space: 114.5 MB (97.97%) - LeetHub

---
+++ 2029-stone-game-ix/2029-stone-game-ix.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean stoneGameIX(int[] stones) {
+        int r1 = 0;
+        int r2 = 0;
+        int r3 = 0;
+
+        for (int n : stones) {
+            if (n % 3 == 0) r1++;
+            else if (n % 3 == 1) r2++;
+            else r3++;
+        }
+
+        // even
+        if (r1 % 2 == 0) {
+            return r2 > 0 && r3 > 0;
+        }
+
+        // odd
+        return Math.abs(r2-r3) > 2;
+    }
+}
