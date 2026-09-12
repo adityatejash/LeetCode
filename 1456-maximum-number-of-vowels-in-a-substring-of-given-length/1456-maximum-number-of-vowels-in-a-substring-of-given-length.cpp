7df004d2958652e5b7bdeb9162f5375a99fb50a7
Aditya Prakash <adityatejash@gmail.com>
2026-09-12 15:55:05 +0530

    Time: 7 ms (51.37%), Space: 17 MB (6.8%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    bool isVowel(char c) {
+    bool isVowel (char c) {
         return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
     }
 
@@ -9,21 +9,17 @@ public:
 
         vector<int> count(n, 0);
 
-        count[0] = isVowel(s[0]) ? 1 : 0;
+        if (isVowel(s[0])) count[0] = 1;
 
-        for (int i = 1; i < n; i++) {
-            count[i] = count[i - 1];
+        for (int i=1; i<n; i++) {
+            count[i] = count[i-1];
 
-            if (isVowel(s[i])) {
-                count[i]++;
-            }
+            if (isVowel(s[i])) count[i]++;
         }
 
-        int ans = 0;
+        int ans = count[k-1];
 
-        ans = count[k - 1];
-
-        for (int j = k; j < n; j++) {
+        for (int j=k; j<n; j++) {
             int i = j - k;
 
             int vowels = count[j] - count[i];
